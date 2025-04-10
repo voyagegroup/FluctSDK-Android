3f98185b1be8829a4bcacc730990ff811d3b4017
FluctMember <fluct_dev@cartahd.com>
2025-04-10 08:37:13 +0000

    add SDK Ver 9.13.1

--- SampleApp/kotlin/app/src/main/java/jp/fluct/sample/kotlinapp/RewardedVideoActivity.kt
+++ SampleApp/kotlin/app/src/main/java/jp/fluct/sample/kotlinapp/RewardedVideoActivity.kt
@@ -14,37 +14,82 @@ import java.util.*
 
 class RewardedVideoActivity : AppCompatActivity() {
 
-    private lateinit var rewardedVideo: FluctRewardedVideo
+    companion object {
 
-    private var stateTextView: TextView? = null
-    private var showButton: Button? = null
+        private const val GROUP_ID = "1000090271"
+        private const val UNIT_ID = "1000135434"
+
+        private const val SAVED_STATE_UI_STATE_TEXT = "ui_state_text"
+        private const val SAVED_STATE_UI_LOAD_BUTTON_ENABLED = "ui_load_button_enabled"
+        private const val SAVED_STATE_UI_SHOW_BUTTON_ENABLED = "ui_show_button_enabled"
+
+    }
+
+    private lateinit var stateTextView: TextView
+    private lateinit var showButton: Button
+    private lateinit var loadButton: Button
+
+    private fun getRewardedVideoInstance(): FluctRewardedVideo {
+        val settings = FluctRewardedVideoSettings.Builder()
+            .testMode(true)
+            .debugMode(true)
+            .build()
+        return FluctRewardedVideo.getInstance(
+            GROUP_ID,
+            UNIT_ID,
+            this,
+            settings
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rewardedvideo)
 
         stateTextView = findViewById<TextView>(R.id.state_textview)
+        showButton = findViewById<Button>(R.id.show_button)
+        loadButton = findViewById<Button>(R.id.load_button)
 
-        val groupID = "1000090271"
-        val unitID = "1000135434"
+        showButton.setOnClickListener {
+            if (getRewardedVideoInstance().isAdLoaded) {
+                getRewardedVideoInstance().show()
+            }
+        }
+        loadButton.setOnClickListener {
+            if (!getRewardedVideoInstance().isAdLoaded) {
+                val targeting = FluctAdRequestTargeting()
+                targeting.gender = FluctAdRequestTargeting.FluctGender.MALE
+                targeting.birthday = GregorianCalendar(1988, Calendar.JANUARY, 1).time
 
-        val settings = FluctRewardedVideoSettings.Builder()
-            .testMode(true)
-            .debugMode(true)
-            .build()
+                getRewardedVideoInstance().loadAd(targeting)
 
-        rewardedVideo = FluctRewardedVideo.getInstance(groupID, unitID, this, settings)
-        rewardedVideo.setListener(object : FluctRewardedVideo.Listener {
+                updateUi(
+                    state = "State: Loading",
+                    loadButtonEnabled = false
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getRewardedVideoInstance().setListener(object : FluctRewardedVideo.Listener {
             // 広告読み込み完了
             override fun onLoaded(groupId: String?, unitId: String?) {
-                showButton?.isEnabled = true
-                updateStateTextView("Loaded")
+                updateUi(
+                    state = "State: Loaded",
+                    loadButtonEnabled = false,
+                    showButtonEnabled = true
+                )
                 Log.d("RewardedVideoActivity", "onLoaded")
             }
 
             // 広告読み込み失敗
             override fun onFailedToLoad(groupId: String?, unitId: String?, errorCode: FluctErrorCode?) {
-                updateStateTextView("Failed to load")
+                updateUi(
+                    state = "State: Failed to load",
+                    loadButtonEnabled = true
+                )
                 Log.d("RewardedVideoActivity", "onFailedToLoad")
             }
 
@@ -65,8 +110,11 @@ class RewardedVideoActivity : AppCompatActivity() {
 
             // 広告が閉じられた
             override fun onClosed(groupId: String?, unitId: String?) {
-                showButton?.isEnabled = false
-                updateStateTextView("Complete")
+                updateUi(
+                    state = "State: Complete",
+                    loadButtonEnabled = true,
+                    showButtonEnabled = false
+                )
                 Log.d("RewardedVideoActivity", "onClosed")
             }
 
@@ -75,24 +123,32 @@ class RewardedVideoActivity : AppCompatActivity() {
                 Log.d("RewardedVideoActivity", "onFailedToPlay")
             }
         })
-
-        showButton = findViewById<Button>(R.id.show_button)
-        showButton?.setOnClickListener {
-            if (rewardedVideo.isAdLoaded) {
-                rewardedVideo.show()
-            }
-        }
-        findViewById<Button>(R.id.load_button).setOnClickListener {
-            val targeting = FluctAdRequestTargeting()
-            targeting.gender = FluctAdRequestTargeting.FluctGender.MALE
-            targeting.birthday = GregorianCalendar(1988, Calendar.JANUARY, 1).time
-            rewardedVideo.loadAd(targeting)
-            updateStateTextView("Loading")
-        }
     }
 
     @SuppressLint("SetTextI18n")
-    fun updateStateTextView(text: String) {
-        stateTextView?.setText("State: " + text)
+    private fun updateUi(
+        state: String,
+        loadButtonEnabled: Boolean = loadButton.isEnabled,
+        showButtonEnabled: Boolean = showButton.isEnabled
+    ) {
+        stateTextView.text = state
+        loadButton.isEnabled = loadButtonEnabled
+        showButton.isEnabled = showButtonEnabled
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SAVED_STATE_UI_STATE_TEXT, stateTextView.text.toString())
+        outState.putBoolean(SAVED_STATE_UI_LOAD_BUTTON_ENABLED, loadButton.isEnabled)
+        outState.putBoolean(SAVED_STATE_UI_SHOW_BUTTON_ENABLED, showButton.isEnabled)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        updateUi(
+            state = savedInstanceState.getString(SAVED_STATE_UI_STATE_TEXT) ?: "",
+            loadButtonEnabled = savedInstanceState.getBoolean(SAVED_STATE_UI_LOAD_BUTTON_ENABLED),
+            showButtonEnabled = savedInstanceState.getBoolean(SAVED_STATE_UI_SHOW_BUTTON_ENABLED)
+        )
     }
 }
